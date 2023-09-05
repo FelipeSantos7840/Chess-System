78a6579b9fb493ae574c5693278d8e969ef7332e
Felipe Dos Santos <felipe.santosmf2022@gmail.com>
2023-09-05 17:22:57 -0300

    Implementado metodos na Board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -27,4 +27,13 @@ public class Board {
     public void setColumns(int columns) {
         this.columns = columns;
     }
+
+    public Piece piece(int row, int column){
+        return pieces[row][column];
+    }
+
+    public Piece piece(Position pos){
+        return pieces[pos.getRow()][pos.getColumn()];
+    }
+
 }
