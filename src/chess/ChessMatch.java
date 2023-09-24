2fe2b5f3c30906dd54fd005fe79de8dd2d7a32ac
Felipe Dos Santos <felipe.santosmf2022@gmail.com>
2023-09-24 00:00:52 -0300

    Moviment Especial Roque (Castling)

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -92,6 +92,26 @@ public class ChessMatch {
             piecesCaptured.add(capturedPiece);
         }
 
+        //SPECIAL MOVE castling Kingside rook
+        if(p instanceof King && target.getColumn() == source.getColumn()+2){
+            Position sourceR = new Position(source.getRow(),source.getColumn()+3);
+            Position targetR = new Position(source.getRow(),source.getColumn()+1);
+
+            ChessPieces rook = (ChessPieces) board.removePiece(sourceR);
+            board.placePiece(rook,targetR);
+            rook.increaseMoveCount();
+        }
+
+        //SPECIAL MOVE castling Queenside rook
+        if(p instanceof King && target.getColumn() == source.getColumn()-2){
+            Position sourceR = new Position(source.getRow(),source.getColumn()-4);
+            Position targetR = new Position(source.getRow(),source.getColumn()-1);
+
+            ChessPieces rook = (ChessPieces) board.removePiece(sourceR);
+            board.placePiece(rook,targetR);
+            rook.increaseMoveCount();
+        }
+
         return capturedPiece;
     }
 
@@ -104,6 +124,26 @@ public class ChessMatch {
             board.placePiece(capturedPiece,target);
             piecesCaptured.remove(capturedPiece);
             piecesOnTheBoard.add(capturedPiece);
+        }
+
+        //SPECIAL MOVE castling Kingside rook
+        if(p instanceof King && target.getColumn() == source.getColumn()+2){
+            Position sourceR = new Position(source.getRow(),source.getColumn()+3);
+            Position targetR = new Position(source.getRow(),source.getColumn()+1);
+
+            ChessPieces rook = (ChessPieces) board.removePiece(targetR);
+            board.placePiece(rook,sourceR);
+            rook.decreaseMoveCount();
+        }
+
+        //SPECIAL MOVE castling Queenside rook
+        if(p instanceof King && target.getColumn() == source.getColumn()-2){
+            Position sourceR = new Position(source.getRow(),source.getColumn()-4);
+            Position targetR = new Position(source.getRow(),source.getColumn()-1);
+
+            ChessPieces rook = (ChessPieces) board.removePiece(targetR);
+            board.placePiece(rook,sourceR);
+            rook.decreaseMoveCount();
         }
     }
 
@@ -200,7 +240,7 @@ public class ChessMatch {
         placeNewPiece('b', 1, new Knight(board, Color.WHITE));
         placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('d', 1, new Queen(board, Color.WHITE));
-        placeNewPiece('e', 1, new King(board, Color.WHITE));
+        placeNewPiece('e', 1, new King(board, Color.WHITE, this));
         placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('g', 1, new Knight(board, Color.WHITE));
         placeNewPiece('h', 1, new Rook(board, Color.WHITE));
@@ -218,7 +258,7 @@ public class ChessMatch {
         placeNewPiece('b', 8, new Knight(board, Color.BLACK));
         placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('d', 8, new Queen(board, Color.BLACK));
-        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('e', 8, new King(board, Color.BLACK,this));
         placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('g', 8, new Knight(board, Color.BLACK));
         placeNewPiece('h', 8, new Rook(board, Color.BLACK));
--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -2,13 +2,17 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPieces;
 import chess.enums.Color;
 
 public class King extends ChessPieces {
 
-    public King(Board board, Color color) {
+    private ChessMatch chessMatch;
+
+    public King(Board board, Color color, ChessMatch chessMatch) {
         super(board, color);
+        this.chessMatch = chessMatch;
     }
 
     @Override
@@ -19,6 +23,11 @@ public class King extends ChessPieces {
     private boolean canMove(Position position){
         ChessPieces piece = (ChessPieces) getBoard().piece(position);
         return piece == null || piece.getColor() != getColor();
+    }
+
+    private boolean testRookCastling(Position position){
+        ChessPieces p = (ChessPieces) getBoard().piece(position);
+        return (p != null && p instanceof Rook && p.getColor() == this.getColor() && p.getMoveCount() == 0);
     }
 
     @Override
@@ -73,6 +82,31 @@ public class King extends ChessPieces {
         p.setValues(position.getRow() + 1 , position.getColumn() + 1);
         if(getBoard().positionExist(p) && canMove(p)){
             mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //SPECIAL MOVE Castling
+        if(this.getMoveCount() == 0 && !chessMatch.getCheck()){
+            //SPECIAL MOVE king side rook
+            Position pRook1 = new Position(position.getRow(),position.getColumn()+3);
+            if(testRookCastling(pRook1)){
+                Position p1 = new Position(position.getRow(),position.getColumn()+1);
+                Position p2 = new Position(position.getRow(),position.getColumn()+2);
+                if(getBoard().piece(p1) == null && getBoard().piece(p2) == null){
+                    mat[position.getRow()][position.getColumn() + 2] = true;
+                }
+            }
+
+            //SPECIAL MOVE queen side rook
+            Position pRook2 = new Position(position.getRow(),position.getColumn()-4);
+            if(testRookCastling(pRook1)){
+                Position p1 = new Position(position.getRow(),position.getColumn()-1);
+                Position p2 = new Position(position.getRow(),position.getColumn()-2);
+                Position p3 = new Position(position.getRow(),position.getColumn()-3);
+
+                if(getBoard().piece(p1) == null && getBoard().piece(p2) == null && getBoard().piece(p3) == null){
+                    mat[position.getRow()][position.getColumn() - 2] = true;
+                }
+            }
         }
 
         return mat;
